b6c566e0801f748e8a930410850492409fde5afe
Harsh_Aakoliya <harshaakoliya20@gmail.com>
2024-10-02 16:01:26 +0530

    Added Merge Sort Snippet for C++

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Box from "./components/box/Box";
 import HelloWorldCode from "./snippets/C++/bubbleSort";
-
+import MergeSort from "./snippets/C++/MergeSort";
 const App = () => {
   return (
     <Box>
-        <HelloWorldCode />
+        {/* <HelloWorldCode /> */}
+        <MergeSort/>
     </Box>
   );
 };
