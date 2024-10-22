cea67d9529c6cd9085fa5bf8924dc7702b5a8fe0
shrinidhi <shrinidhis007@gmail.com>
2024-10-22 22:47:51 +0530

    fix

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -31,6 +31,7 @@ const Dashboard = ({ searchTerm }: { searchTerm: string }) => {
     { title: "SHA-3 Hashing Algorithm", route: "/sha3-hash" },
     { title: "OAuth2", route: "/OAuth2_Authorization" },
     { title: "Advanced Encryption Standard (AES)", route: "/aes" },
+    { title: "Playfair Cipher", route: "/playfaircipher" },
   ];
 
   const filteredCards = cards.filter((card) =>
@@ -40,35 +41,6 @@ const Dashboard = ({ searchTerm }: { searchTerm: string }) => {
     <div>
       <div className="w-full flex flex-col items-center">
         <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
-          <Card title="Bubble Sort" route="/bubblesort" />
-          <Card title="Merge Sort" route="/mergesort" />
-          <Card title="Heap Sort" route="/heapsort" />
-          <Card title="A Star" route="/astar" />
-          <Card title="Binary Search" route="/binaysearch" />
-          <Card title="Depth First Search" route="/dfs" />
-          <Card title="Breadth First Search" route="/bfs" />
-          <Card title="Bellman Ford" route="/bellmanford" />
-          <Card
-            title="Rate Limiting Middleware"
-            route="/rateLimitingMiddleware"
-          />
-          <Card title="Debounce Function" route="/debounceFunction" />
-          <Card title="Chaining Promises" route="/chainingPromises" />
-          <Card title="Dijkstra Algorithm" route="/dijkstraalgorithm" />
-          <Card title="Jwt Authentication Flow" route="/jwtauthentication" />
-          <Card title="Thread Synchronization" route="/threadsynchronization" />
-          <Card
-            title="Thread Synchronization Java"
-            route="/threadsynchronizationJava"
-          />
-
-          <Card title="Fetch API" route="/fetchAPI" />
-          <Card title="Axios" route="/axios" />
-          <Card title="XMLHttpRequest" route="/xmlhttprequest" />
-          <Card title="WebSocket" route="/WebSocket" />
-          <Card title="Blowfish Cipher" route="/blowfishcipher" />
-          <Card title="Hill Cipher" route="/hillcipher" />
-          <Card title="Playfair Cipher" route="/playfaircipher" />
           <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
             {filteredCards.map((card) => (
               <Card key={card.title} title={card.title} route={card.route} />
